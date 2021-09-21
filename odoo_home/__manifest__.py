f84b344d2e125434f0cba3357597516ae0cfa289
Fanch <fle@apik.cloud>
2021-09-21 11:14:44 +0200

    ajout odoo home automation

---
+++ odoo_home/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Odoo Home Automation",
+    "version": "1.0",
+    "author": "François Le Gal",
+    "website": "https://github.com/fanchlegal/odoobox",
+    "license": "AGPL-3",
+    "category": "Vertical",
+    "description": "Home automation for Odoo",
+    "depends": [
+        'base',
+    ],
+    "data": [
+        'security/ir.model.access.csv',
+    ],
+    "demo": [
+	    
+    ],
+    'qweb': [
+	    
+    ],
+    'installable': True,
+	'application':True,
+}
