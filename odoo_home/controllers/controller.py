d0cb65e8e94d31776f077abbde3f1e28f7ed267c
Fanch <fle@apik.cloud>
2022-04-29 20:27:31 +0000

    maj

--- odoo_home/controllers/controller.py
+++ odoo_home/controllers/controller.py
@@ -1,2 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import json
+
+from odoo import exceptions, _
+from odoo.http import Controller, request, route
+
+
+class HomeBusController(Controller):
+	
+	
+	@http.route('/lonpolling/post', type='http', auth="public")
+	def longpolling_post(self,login,password,channel,type,message):
+		try:
+			uid = request.session.authenticate(request.session.db, request.params['login'], request.params['password'])
+			
+			request.env['bus.bus']._sendone(request.params['channel'],request.params['type'],request.params['message'])
+			
+			
+			
+			
+		except odoo.exceptions.AccessDenied as e:
+			logger.info(str(e))
+			return request.not_found()
+			
+		return True
--- odoo_home/models/bus.py
+++ odoo_home/models/bus.py
@@ -5,10 +5,3 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class bus_bus(models.Model):
-	_name = 'bus.bus'
-	_inherit = 'bus.bus'
-	
-	@api.model
-	def sendone(self, channel, notification_type, message):
-		self._sendmany([[channel, notification_type, message]])
